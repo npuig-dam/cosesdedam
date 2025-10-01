b8f2fb7bcf0c67232ddf3120acbde5730e139c83
barotraumaenjoyer <npuig.dam@institutcampalans.net>
2025-10-01 19:24:57 +0200

    tercer

--- zezzio1/MainWindow.xaml.cs
+++ zezzio1/MainWindow.xaml.cs
@@ -23,6 +23,37 @@ namespace zezzio1
         public MainWindow()
         {
             InitializeComponent();
+
+            Grid greed = new Grid();
+            this.Content = greed;
+
+            Button buto = new Button();
+            //greed.Children.Add(culon);
+            WrapPanel panell = new WrapPanel(); 
+
+            TextBlock blocdetecst = new TextBlock();
+            blocdetecst.Text = "AAAAAAAAAAAAAAAAAAAAAAAAAA ";
+            panell.Children.Add(blocdetecst);
+
+            TextBlock blocdetecst21 = new TextBlock();
+            blocdetecst21.Text = "BBBBBBBBBBBBBBBBBBBBBBB ";
+            panell.Children.Add(blocdetecst21);
+
+            TextBlock blocdetecst7 = new TextBlock();
+            blocdetecst7.Text = "CCCCCCCCCCCCCCCCCCCCCCCCC";
+            panell.Children.Add(blocdetecst7);
+
+            buto.Content = panell;
+            greed.Children.Add(buto);
+
+            buto.Height = 200;
+            buto.Width = 150;
+            buto.Background = Brushes.Aquamarine;
+
+            blocdetecst.Foreground = Brushes.Pink;
+            blocdetecst21.Foreground = Brushes.Yellow;
+            blocdetecst7.Foreground = Brushes.LightBlue;
+
         }
     }
 }
